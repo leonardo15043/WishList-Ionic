b0c35b595f77b385ae179034bec79b5f389361e2
leonardo15043 <lahernandez15043@gmail.com>
2019-01-20 14:47:05 -0500

    Editar titulo de la lista

--- src/components/listas.component.ts
+++ src/components/listas.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { DeseosService } from '../services/deseos.services';
 import { Lista } from '../models/index';
 import { AgregarPage } from '../pages/agregar/agregar.component';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ItemSliding } from 'ionic-angular';
 
 @Component({
   selector: 'app-listas',
@@ -14,7 +14,8 @@ export class ListasComponent {
 
   constructor(
     public deseosService:DeseosService,
-    private navCtr:NavController
+    private navCtr:NavController,
+    private alertCtr: AlertController
   ) {  }
 
   listaSeleccionada(lista: Lista){
@@ -28,6 +29,36 @@ export class ListasComponent {
 
   borrarLista( lista: Lista ){
     this.deseosService.borrarLista( lista );
+  }
+
+  editarLista( lista:Lista , slidingItem:ItemSliding){
+
+    slidingItem.close();
+
+    const alerta = this.alertCtr.create({
+      title: "Editar nombre",
+      message: "Editar el nombre de la lista",
+      inputs:[{
+        name: 'titulo',
+        placeholder: 'Nombre de la lista',
+        value: lista.titulo
+      }],
+      buttons:[{
+        text: 'Guardar',
+        handler: data =>{
+          if(data.titulo.length === 0){
+            return;
+          }
+
+          lista.titulo = data.titulo;
+          this.deseosService.guardarStorage();
+
+        }
+      }]
+    });
+
+    alerta.present();
+
   }
 
 
